3f80a2d43c103eb5d81075ee6cb84ef8bf053611
Vlad Furtuna <144479033+furtunavlad@users.noreply.github.com>
2024-03-10 00:18:56 +0200

    fill listview with mock data

---
+++ Appetizers/Appetizer.swift
@@ -0,0 +1,36 @@
+//
+//  Appetizer.swift
+//  Appetizers
+//
+//  Created by Vlad Furtuna on 09.03.2024.
+//
+
+import Foundation
+
+struct Appetizer: Decodable, Identifiable {
+    let id: Int
+    let name: String
+    let desciption: String
+    let price: Double
+    let imageURL: String
+    let calories: Int
+    let protein: Int
+    let carbs: Int
+}
+
+struct AppetizerResponse {
+    let request: [Appetizer]
+}
+
+struct MockData {
+    static let sampleAppetizer = Appetizer(id: 001,
+                                           name: "test appetizer",
+                                           desciption: "this is the description for my appetizer",
+                                           price: 10.99,
+                                           imageURL: "",
+                                           calories: 250,
+                                           protein: 59,
+                                           carbs: 80)
+    
+    static let appetizers = [sampleAppetizer, sampleAppetizer, sampleAppetizer, sampleAppetizer]
+}
--- Appetizers/AppetizerListView.swift
+++ Appetizers/AppetizerListView.swift
@@ -9,7 +9,29 @@ import SwiftUI
 
 struct AppetizerListView: View {
     var body: some View {
-        Text("Appetizer List View")
+        NavigationView {
+            List(MockData.appetizers) { appetizer in
+                HStack {
+                    Image("asian-flank-steak")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 120, height: 90)
+                        .cornerRadius(8)
+                    
+                    VStack(alignment: .leading, spacing: 5){
+                        Text(appetizer.name)
+                            .font(.title2)
+                            .fontWeight(.medium)
+                        
+                        Text("$\(appetizer.price, specifier: "%.2f")")
+                            .foregroundColor(.secondary)
+                            .fontWeight(.semibold)
+                    }
+                    .padding(.leading)
+                }
+            }
+            .navigationTitle("🍟 Appetizers")
+        }
     }
 }
 
