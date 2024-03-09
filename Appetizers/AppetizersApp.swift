a0e6b30f161389a54b809e520cf87d78d68a36e5
Vlad Furtuna <144479033+furtunavlad@users.noreply.github.com>
2024-03-09 20:47:51 +0200

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Vlad Furtuna on 09.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
