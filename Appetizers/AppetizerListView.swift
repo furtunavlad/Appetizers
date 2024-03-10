b6c7b6c9bb906481dbef63f3f9ed449a0f68b99b
Vlad Furtuna <144479033+furtunavlad@users.noreply.github.com>
2024-03-10 18:33:28 +0200

    NetworkManager and Errors

--- Appetizers/AppetizerListView.swift
+++ Appetizers/AppetizerListView.swift
@@ -11,24 +11,8 @@ struct AppetizerListView: View {
     var body: some View {
         NavigationView {
             List(MockData.appetizers) { appetizer in
-                HStack {
-                    Image("asian-flank-steak")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 120, height: 90)
-                        .cornerRadius(8)
-                    
-                    VStack(alignment: .leading, spacing: 5){
-                        Text(appetizer.name)
-                            .font(.title2)
-                            .fontWeight(.medium)
-                        
-                        Text("$\(appetizer.price, specifier: "%.2f")")
-                            .foregroundColor(.secondary)
-                            .fontWeight(.semibold)
-                    }
+                AppetizerListCell(appetizer: appetizer)
                     .padding(.leading)
-                }
             }
             .navigationTitle("🍟 Appetizers")
         }
